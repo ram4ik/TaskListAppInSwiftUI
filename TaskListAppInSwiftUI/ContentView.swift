c3b9312eed0a42495bdd08f2480f75083879f601
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-22 12:27:38 +0200

    Initial Commit

---
+++ TaskListAppInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskListAppInSwiftUI
+//
+//  Created by ramil on 22.12.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskListAppInSwiftUI/TaskListAppInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListAppInSwiftUIApp.swift
+//  TaskListAppInSwiftUI
+//
+//  Created by ramil on 22.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TaskListAppInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
