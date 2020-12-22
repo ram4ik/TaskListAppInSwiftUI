03b4f620bb22d5b24262ef8298652b989fb76db0
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-12-22 12:42:34 +0200

    tasks data model with cool tip

--- TaskListAppInSwiftUI/Models/TasksStore.swift
+++ TaskListAppInSwiftUI/Models/TasksStore.swift
@@ -9,4 +9,20 @@ import Foundation
 
 class TasksStore {
     
+    var tasks = [
+        TaskDataModel(name: "Learn SwiftUI"),
+        TaskDataModel(name: "Learn Combine"),
+        TaskDataModel(name: "Learn UIKit"),
+        TaskDataModel(name: "Do morning exercise"),
+        TaskDataModel(name: "Drink more water"),
+        TaskDataModel(name: "Sleep well"),
+        TaskDataModel(name: "More walking"),
+    ]
+    // Cool tip: use .map{} to generate array
+    /*
+     var task = [
+        "New Task One",
+        "New Task Two",
+     ]. map { TaskDataModel(name: $0) }
+     */
 }
